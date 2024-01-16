d201d6d0d9170c30d852a2c2e677e90bacaffca4
Bob Xplosion <bob0011@hotmail.com>
2024-01-15 21:49:43 -0600

    menu mobile

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,18 +1,52 @@
+import { useState } from 'react';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [openMenu, setOpenMenu] = useState(false)
+
+  const toggleMenu = () => {
+    setOpenMenu(!openMenu)
+  }
+
   return (
     <section className='navbar container'>
-      <h1>codeBrewers</h1>
-      <div className='links'>
-        <a href='#about-us'>About Us</a>
-        <a href='#traits'>Traits</a>
-        <a target='_blanck' href='https://www.linkedin.com/feed/'>
-          LinkedIn
-        </a>
-        <a href='/contact-us' className='get-started-button'>
-          Get Started
-        </a>
+      <div className="desktop-menu">
+        <h1>codeBrewers</h1>
+        <div className='links'>
+          <a href='#about-us'>About Us</a>
+          <a href='#traits'>Traits</a>
+          <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/feed/'>
+            LinkedIn
+          </a>
+          <a href='/contact-us' className='get-started-button'>
+            Get Started
+          </a>
+        </div>
+      </div>
+      <div className="mobile-menu">
+        <h1>codeBrewers</h1>
+        <div
+          className='mobile-menu-toggle'
+          onClick={toggleMenu}
+        >
+          <img
+            className="mobile-menu-icon"
+            src="/img/menu-burger.svg"
+            alt="Menu Icon" />
+        </div>
+        { openMenu && 
+          <div className='mobile-links'>
+            <a href='#about-us'>About Us</a>
+            <a href='#traits'>Traits</a>
+            <a target='_blank' rel="noreferrer" href='https://www.linkedin.com/feed/'>
+              LinkedIn
+            </a>
+            <a href='/contact-us' className='get-started-button-mobile'>
+              Get Started
+            </a>
+          </div>
+        }
+
       </div>
     </section>
   );
